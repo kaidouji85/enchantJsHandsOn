bcfabf5b6c97805c5294317f804ee163bdf352da
kaidouji85 <kaidouji85@gmail.com>
2014-09-02 18:05:19 +0900

    プレイヤーが画面外に移動するのを防止した。

---
+++ 004_fixOutOfScreen/game.js
@@ -0,0 +1,48 @@
+enchant();
+
+var core;
+var player;
+
+window.onload = function() {
+    core = new Core(640, 480);
+    core.fps = 60;
+    core.rootScene.backgroundColor = "black";
+
+    core.preload('player.PNG');
+
+    core.onload = initGame;
+    core.start();
+};
+
+function initGame() {
+    player = new Sprite(128,128);
+    player.image = core.assets['player.PNG'];
+    player.x = 256;
+    player.y = 352;
+    player.addEventListener(Event.ENTER_FRAME,function(){
+        if(core.input.right){
+            player.x += 4;
+        } else if(core.input.left) {
+            player.x -= 4;
+        }
+
+        if(core.input.up){
+            player.y -= 4;
+        } else if(core.input.down) {
+            player.y += 4;
+        }
+
+        if(player.x < 0){
+            player.x = 0;
+        }if(player.x > 512) {
+            player.x = 512;
+        }
+        if(player.y < 0){
+            player.y = 0;
+        }
+        if(player.y > 352){
+            player.y = 352;
+        }
+    });
+    core.rootScene.addChild(player);
+}
